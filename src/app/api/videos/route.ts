242ab9dbae160d452fba996b033da9dbfa01b273
nipun <nipun.chathuranga.eds@gmail.com>
2023-12-14 21:13:12 +0530

    FIX deployment error:boom:

--- src/app/api/videos/route.ts
+++ src/app/api/videos/route.ts
@@ -8,7 +8,7 @@ export async function GET(request: NextRequest) {
   let response = { error: false, data: {} };
 
   try {
-    const id: number = Number(request.nextUrl.searchParams.get("id") || 0);
+    const id: number = Number(2);
 
     const videos = await getAllVideos(id);
     response.data = videos;
