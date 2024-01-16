40b6b358e519682c86c136d37ef48bff7d7f6c9d
nipun <nipun.chathuranga.eds@gmail.com>
2024-01-16 09:59:00 +0530

    test token 4

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,8 +10,6 @@ const Navbar = () => {
   const router = useRouter();
   React.useEffect(() => {
     const token = localStorage.getItem("token");
-    const parsedToken = token ? JSON.parse(token) : null;
-    alert(parsedToken);
 
     if (token)
       useStore.setState((prev) => ({ ...prev, isAuthenticated: true }));
